857acdacb4c34368ab2086aaf70cb2c6d1331ceb
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-08-04 23:25:09 +0530

    MAking Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { lazy } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
+const Home = lazy(() => import("./Pages/Homepage"));
+const Contact = lazy(() => import("./Pages/Contactpage"));
+const Projects = lazy(() => import("./Pages/Projectpage"));
+
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "contact",
+      element: <Contact />,
+    },
+    {
+      path: "projects",
+      element: <Projects />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
