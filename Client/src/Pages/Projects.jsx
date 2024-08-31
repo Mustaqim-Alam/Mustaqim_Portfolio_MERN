cf3c56bcc30233e3461e2133ca1c9b9aee289678
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-09-01 01:19:55 +0530

    MAking Portfolio

--- Client/src/Pages/Projects.jsx
+++ Client/src/Pages/Projects.jsx
@@ -5,85 +5,120 @@ import { PiGithubLogoLight } from "react-icons/pi";
 const Projects = () => {
   return (
     <div className="bg-red-300 flex justify-center">
-      <div className=" flex flex-col max-w-6xl px-4  items-center bg-green-300">
-
-     
-      <div className=" bg-slate-400  max ">
-        <div>
-          <button>Showcase</button>
-          <h1>Explore my projects</h1>
-          <p>
-            I love to build stuff. Here are some projects that i have worked on.
-          </p>
-        </div>
-      </div>
-      <div>
-        <button>
-          <span>All Projects</span>
-          <BiCode />
-        </button>
-      </div>
-      <div>
-        <div>
-          <img
-            src="https://www.singlegrain.com/wp-content/uploads/2019/03/SG-The-4-Fundamentals-of-E-commerce-Website-Design.jpg"
-            alt=""
-          />
-          <h1>ShoppingKart - MERN Stack E-commerce App</h1>
-          <p>
-            E-commerce admin dashboard with dynamic product management, secure
-            APIs, and data visualization tools.
-          </p>
-          <ul>
-            <li>ReactJS</li>
-            <li>NodeJS</li>
-            <li>MongoDB</li>
-            <li>ExpressJs</li>
-            <li>Sass</li>
-            <li>Redux</li>
-          </ul>
+      <div className=" flex flex-col max-w-5xl px-8  items-center bg-green-300">
+        <div className=" h-52 w-full flex justify-start items-start px-4 pt-10   gap-2 max ">
           <div>
-            <button>
-              <MdOutlineOpenInNew />
-              <span>Website</span>
+            <button className=" py-1 px-4 bg-bgColor1 text-whiteColor font-fontf2 rounded-lg">
+              Showcase
             </button>
-            <button>
-              <PiGithubLogoLight />
-              <span>Source</span>
-            </button>
+            <h1 className=" font-semibold text-3xl">Explore my projects</h1>
+            <p className=" font-fontf3">
+              I love to build stuff. Here are some projects that i have worked
+              on.
+            </p>
+          </div>
+        </div>
+        <div className=" flex w-full justify-end">
+          <button className=" border-2 border-borderColor2 rounded-md bg-bgColor2 flex w-44 py-1 px-1 items-center justify-around">
+            <span>All Projects</span>
+            <BiCode />
+          </button>
+        </div>
+        <div className=" w-full bg-red-300 grid grid-cols-2 gap-4 px-8">
+          <div className=" border-2 border-borderColor2 rounded-xl w-full bg-lime-300">
+            <img
+              className=" w-full pt-4 pl-4  rounded-tl-3xl"
+              src="https://www.singlegrain.com/wp-content/uploads/2019/03/SG-The-4-Fundamentals-of-E-commerce-Website-Design.jpg"
+              alt=""
+            />
+            <div className=" p-1 bg-bgColor1 text-whiteColor">
+              <h1 className=" text-xl font-fontf3">ShoppingKart - MERN Stack E-commerce App</h1>
+              <p className=" text-sm font-fontf3">
+                E-commerce admin dashboard with dynamic product management,
+                secure APIs, and data visualization tools.
+              </p>
+              <ul>
+                <li>ReactJS</li>
+                <li>NodeJS</li>
+                <li>MongoDB</li>
+                <li>ExpressJs</li>
+                <li>Sass</li>
+                <li>Redux</li>
+              </ul>
+              <div>
+                <button>
+                  <MdOutlineOpenInNew />
+                  <span>Website</span>
+                </button>
+                <button>
+                  <PiGithubLogoLight />
+                  <span>Source</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className=" border-2 border-borderColor2 rounded-xl w-full bg-lime-300">
+            <img
+              className=" w-full"
+              src="https://www.singlegrain.com/wp-content/uploads/2019/03/SG-The-4-Fundamentals-of-E-commerce-Website-Design.jpg"
+              alt=""
+            />
+            <h1>ShoppingKart - MERN Stack E-commerce App</h1>
+            <p>
+              E-commerce admin dashboard with dynamic product management, secure
+              APIs, and data visualization tools.
+            </p>
+            <ul>
+              <li>ReactJS</li>
+              <li>NodeJS</li>
+              <li>MongoDB</li>
+              <li>ExpressJs</li>
+              <li>Sass</li>
+              <li>Redux</li>
+            </ul>
+            <div>
+              <button>
+                <MdOutlineOpenInNew />
+                <span>Website</span>
+              </button>
+              <button>
+                <PiGithubLogoLight />
+                <span>Source</span>
+              </button>
+            </div>
+          </div>
+          <div className=" border-2 border-borderColor2 rounded-xl w-full bg-lime-300">
+            <img
+              className=" w-full"
+              src="https://www.singlegrain.com/wp-content/uploads/2019/03/SG-The-4-Fundamentals-of-E-commerce-Website-Design.jpg"
+              alt=""
+            />
+            <h1>ShoppingKart - MERN Stack E-commerce App</h1>
+            <p>
+              E-commerce admin dashboard with dynamic product management, secure
+              APIs, and data visualization tools.
+            </p>
+            <ul>
+              <li>ReactJS</li>
+              <li>NodeJS</li>
+              <li>MongoDB</li>
+              <li>ExpressJs</li>
+              <li>Sass</li>
+              <li>Redux</li>
+            </ul>
+            <div>
+              <button>
+                <MdOutlineOpenInNew />
+                <span>Website</span>
+              </button>
+              <button>
+                <PiGithubLogoLight />
+                <span>Source</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <img
-          src="https://www.singlegrain.com/wp-content/uploads/2019/03/SG-The-4-Fundamentals-of-E-commerce-Website-Design.jpg"
-          alt=""
-        />
-        <h1>ShoppingKart - MERN Stack E-commerce App</h1>
-        <p>
-          E-commerce admin dashboard with dynamic product management, secure
-          APIs, and data visualization tools.
-        </p>
-        <ul>
-          <li>ReactJS</li>
-          <li>NodeJS</li>
-          <li>MongoDB</li>
-          <li>ExpressJs</li>
-          <li>Sass</li>
-          <li>Redux</li>
-        </ul>
-        <div>
-          <button>
-            <MdOutlineOpenInNew />
-            <span>Website</span>
-          </button>
-          <button>
-            <PiGithubLogoLight />
-            <span>Source</span>
-          </button>
-        </div>
-      </div>
-    </div>
     </div>
   );
 };
