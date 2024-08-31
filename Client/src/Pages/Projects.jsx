a951026b965c80faa1c967bcc6626ec926913ce3
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-08-31 23:34:37 +0530

    MAking Portfolio

--- Client/src/Pages/Projects.jsx
+++ Client/src/Pages/Projects.jsx
@@ -4,8 +4,11 @@ import { PiGithubLogoLight } from "react-icons/pi";
 
 const Projects = () => {
   return (
-    <div className="bg-red-300">
-      <div>
+    <div className="bg-red-300 flex justify-center">
+      <div className=" flex flex-col max-w-6xl px-4  items-center bg-green-300">
+
+     
+      <div className=" bg-slate-400  max ">
         <div>
           <button>Showcase</button>
           <h1>Explore my projects</h1>
@@ -80,6 +83,7 @@ const Projects = () => {
           </button>
         </div>
       </div>
+    </div>
     </div>
   );
 };
