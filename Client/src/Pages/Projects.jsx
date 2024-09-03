37db0e89d0716d0e32e40a463ed11206cdb0df85
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-09-03 22:58:07 +0530

    Portfolio making

--- Client/src/Pages/Projects.jsx
+++ Client/src/Pages/Projects.jsx
@@ -3,6 +3,16 @@ import { MdOutlineOpenInNew } from "react-icons/md";
 import { PiGithubLogoLight } from "react-icons/pi";
 
 const Projects = () => {
+  const productData = [
+    {
+      img: "https://www.singlegrain.com/wp-content/uploads/2019/03/SG-The-4-Fundamentals-of-E-commerce-Website-Design.jpg",
+      name: "ShoppingKart - MERN Stack E-commerce App",
+      description:
+        " E-commerce admin dashboard with dynamic product management,secure APIs, and data visualization tools.",
+      techstack: ["ReactJS", "NodeJS", "MongoDB", "ExpressJS", "SCSS"],
+    },
+  ];
+
   return (
     <div className=" bg-bgColor2 flex justify-center">
       <div className=" flex flex-col max-w-5xl px-8  items-center ">
@@ -25,92 +35,60 @@ const Projects = () => {
           </button>
         </div>
         <div className=" w-full  grid grid-cols-2 gap-4 px-8 py-4">
-          <div className=" border-2 border-borderColor2 rounded-xl w-80 h-max bg-lime-300">
-            <img
-              className=" w-full h-40 pt-4 pl-4  rounded-tl-3xl"
-              src="https://www.singlegrain.com/wp-content/uploads/2019/03/SG-The-4-Fundamentals-of-E-commerce-Website-Design.jpg"
-              alt=""
-            />
-            <div className=" p-1 bg-white text-black">
-              <h1 className=" text-lg font-semibold font-fontf3">
-                ShoppingKart - MERN Stack E-commerce App
-              </h1>
-              <p className="px-2 text-sm font-fontf3">
-                E-commerce admin dashboard with dynamic product management,
-                secure APIs, and data visualization tools.
-              </p>
-              <ul className=" flex  px-2 w-full items-center">
-                <li className=" bg-gray-50 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
-                  ReactJS
-                </li>
-                <li className=" bg-gray-50 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
-                  NodeJS
-                </li>
-                <li className=" bg-gray-50 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
-                  MongoDB
-                </li>
-                <li className=" bg-gray-50 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
-                  ExpressJs
-                </li>
-                <li className=" bg-gray-50 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
-                  Sass
-                </li>
-                <li className=" bg-gray-50 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
-                  Redux
-                </li>
-              </ul>
-              <div className=" flex gap-2 m-4">
-                <button className=" flex py-2 px-4 bg-black text-white rounded-lg gap-2 items-center ">
-                  <MdOutlineOpenInNew />
-                  <span>Website</span>
-                </button>
-                <button className=" flex py-2 px-4 bg-black text-white rounded-lg gap-2 items-center ">
-                  <PiGithubLogoLight />
-                  <span>Source</span>
-                </button>
+          {productData.map((product) => {
+            <div>
+              <img src={product.img} alt="" />
+              <div>
+                <h1>{product.name}</h1>
+                <p>{product.description}</p>
+                <ul>
+                  {product.techstack.map((li, i) => {
+                    <li key={i}>{li}</li>;
+                  })}
+                </ul>
               </div>
-            </div>
-          </div>
-          <div className=" border-2 border-borderColor2 rounded-xl w-80 h-max bg-lime-300">
+            </div>;
+          })}
+          <div className=" border-2 border-borderColor2 rounded-xl w-92 h-max bg-lime-300">
             <img
-              className=" w-full pt-4 pl-4  rounded-tl-3xl"
+              className=" w-full h-44 pt-4 pl-4  rounded-tl-3xl"
               src="https://www.singlegrain.com/wp-content/uploads/2019/03/SG-The-4-Fundamentals-of-E-commerce-Website-Design.jpg"
               alt=""
             />
-            <div className=" p-1 bg-white text-black">
+            <div className=" pl-2 h-40 bg-white rounded-b-xl   flex items-start justify-around flex-col text-black">
               <h1 className=" text-lg font-semibold font-fontf3">
                 ShoppingKart - MERN Stack E-commerce App
               </h1>
-              <p className="px-2 text-sm font-fontf3">
+              <p className=" text-sm font-fontf3">
                 E-commerce admin dashboard with dynamic product management,
                 secure APIs, and data visualization tools.
               </p>
-              <ul className=" flex justify-around px-2">
-                <li className=" bg-gray-200 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
+              <ul className=" flex   w-full items-center gap-2 ">
+                <li className=" bg-gray-100 text-blackColor rounded-md text-sm  flex items-center  border px-1 border-gray ">
                   ReactJS
                 </li>
-                <li className=" bg-gray-200 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
+                <li className=" bg-gray-100 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-gray ">
                   NodeJS
                 </li>
-                <li className=" bg-gray-200 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
+                <li className=" bg-gray-100 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-gray ">
                   MongoDB
                 </li>
-                <li className=" bg-gray-200 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
+                <li className=" bg-gray-100 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-gray ">
                   ExpressJs
                 </li>
-                <li className=" bg-gray-200 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
+                <li className=" bg-gray-100 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-gray ">
                   Sass
                 </li>
-                <li className=" bg-gray-200 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-black ">
+                <li className=" bg-gray-100 text-blackColor rounded-md text-sm font-semibold flex items-center  border px-1 border-gray ">
                   Redux
                 </li>
               </ul>
-              <div className=" flex gap-2 m-4">
-                <button className=" flex py-2 px-4 bg-black text-white rounded-lg gap-2 items-center ">
+              <div className=" flex gap-2 ">
+                <button className=" flex py-1 px-4 bg-black text-white rounded-lg gap-2 items-center ">
                   <MdOutlineOpenInNew />
                   <span>Website</span>
                 </button>
-                <button className=" flex py-2 px-4 bg-black text-white rounded-lg gap-2 items-center ">
+                <button className=" flex py-1 px-4 bg-black text-white rounded-lg gap-2 items-center ">
                   <PiGithubLogoLight />
                   <span>Source</span>
                 </button>
