bfca668d2ac4b4019665fca90fa3577eae81ddc1
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-09-04 15:28:01 +0530

    Making Portfolio

--- Client/src/Pages/Experience.jsx
+++ Client/src/Pages/Experience.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
-
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <div className=" w-full h-screen flex flex-col justify-start gap-4 items-center">
+      <div className=" w-2/3 bg-amber-300 ">
+        <div className="h-52 w-full flex justify-start items-start pt-10   gap-2 max">
+          <div>
+            <button className=" py-1 px-4 bg-bgColor3 text-whiteColor font-fontf2 rounded-lg">
+              My Experience
+            </button>
+            <h1 className=" font-semibold text-3xl">Check out my jurney</h1>
+            <p className=" font-fontf3">
+              Look out my jurney and the companies that I have worked with.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+    <div 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    >
+      Hello
+    </div>
+      </div>
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
